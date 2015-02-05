85adc3427b344e42ea440ff246f5dca033efb3b9
andy <andygmb1@gmail.com>
2015-02-05 11:26:17 +0000

    removing commented out url

--- run.py
+++ run.py
@@ -2,6 +2,3 @@ from blog import app
 
 if __name__ == "__main__":
 	app.run(debug=True)
-#https://github.com/mrjoes/flask-admin/blob/master/examples/auth/app.py
-#https://github.com/mrjoes/flask-admin/blob/master/examples/auth/app.py
-#https://github.com/mrjoes/flask-admin/blob/master/examples/auth/app.py
